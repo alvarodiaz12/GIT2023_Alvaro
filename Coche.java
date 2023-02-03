6e3aaabcc5bdc3a79cb1ed6854fdcb206f1b29de
Alvaro <a-alvaro.diaz.casano@iespablopicasso.es>
2023-02-03 13:34:41 +0100

    Nuevo método

--- Coche.java
+++ Coche.java
@@ -1,14 +1,14 @@
 
 public class Coche {
 
-  private string  marca;
+  private string marca;
   private string modelo;
   private string matricula;
-  
-  public coche(String marca,String modelo, String matricula){
-    this marca = marca;
-    this modelo = modelo;
-    this matricula = matricula;
-  }
- 
+  private string puertas
+
+  this marca = marca;
+  this modelo = modelo;
+  this matricula = matricula;
+  this puertas = puertas;
+  hola
 }
